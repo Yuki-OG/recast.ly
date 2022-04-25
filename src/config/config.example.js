46336f4f4a5d7776e58ebb98681aaf4db98268f6
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-26 08:38:27 +0900

    Completed dynamic video list component

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -1,5 +1,5 @@
 // Put your API key goes here!
-var API_KEY = 'YOUR_API_KEY_HERE';
-var YOUTUBE_API_KEY = 'YOUR_YOUTUBE_API_KEY_HERE';
+var API_KEY = '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';
+var YOUTUBE_API_KEY = 'AIzaSyB_04HGbn5u5O44flXWVjBTRFb9_u-Dyb8';
 
 export { API_KEY, YOUTUBE_API_KEY };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+console.log('hello');
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+ReactDOM.render(<App />, document.getElementById('app'));
